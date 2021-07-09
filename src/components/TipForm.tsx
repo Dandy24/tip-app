6cc5517e125b28f4b240ed3be90e3b4116dde2c1
Dan Godula <dan.godula@seznam.cz>
2021-07-09 17:31:51 +0200

    Added some Ant Design styling into a form

--- src/components/TipForm.tsx
+++ src/components/TipForm.tsx
@@ -1,4 +1,7 @@
 import SelectList from './SelectList'
+//import Button from "./Button";
+import {InputNumber, Button, Space} from "antd";
+import {useRef} from "react";
 
 function TipForm(){
 
@@ -16,20 +19,54 @@ function TipForm(){
             'desc': "Perfektní, jsem nadšený"
         }]
 
+    const amountRef = useRef<HTMLInputElement>(null);
+    const qualityRef = useRef();
+    const peopleNoRef = useRef<HTMLInputElement>(null);
+
+    function submitHandler(event: { preventDefault: () => void; }){
+        event.preventDefault();
+
+        // @ts-ignore
+        const enteredAmount = amountRef.current.value;
+        // @ts-ignore
+        const enteredQuality = qualityRef.current.value;
+        // @ts-ignore
+        const enteredPeopleNo = peopleNoRef.current.value;
+
+        const tipData = {
+            amount : enteredAmount,
+            quality : enteredQuality,
+            peopleNo : enteredPeopleNo
+        }
+        console.log(tipData)
+    }
+
+    // @ts-ignore
     return(
-        <form>
+        <form onSubmit={submitHandler}>
+            <Space direction='vertical' size='large'>
             <div>
+                <Space size='large'>
                 <label htmlFor='amount'>Částka na účtu</label>
-                <input type="number" required id='amount'/>
+                <InputNumber required id='amount' ref={amountRef}></InputNumber>
+                </Space>
             </div>
             <div>
+                <Space size='large'>
                 <label htmlFor='quality'>Kvalita obsluhy</label>
-                <SelectList data={service}/>
+                <SelectList data={service} reference={qualityRef}/>
+                </Space>
             </div>
             <div>
+                <Space size='large'>
                 <label htmlFor='peopleNo'>Kolik lidí se dělí o účet</label>
-                <input type="number" required id='peopleNo'/>
+                <InputNumber required id='peopleNo' ref={peopleNoRef}></InputNumber>
+                </Space>
             </div>
+            <div>
+                <Button type='primary' htmlType='submit'>Vypočítat</Button>
+            </div>
+            </Space>
         </form>
     )
 }
