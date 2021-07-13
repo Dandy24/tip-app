f94f40a6bd9cec1deb3f8102b9240a8526b392bc
Dan Godula <dan.godula@seznam.cz>
2021-07-13 15:14:23 +0200

    HistoryList is now updated every time a new calculation is made

--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -1,74 +1,67 @@
-import {HistoryItem} from "./HistoryItem";
-import {Divider} from "antd";
-import {useEffect, useState} from "react";
+import { Divider } from 'antd';
+import { useEffect, useState } from 'react';
+import { HistoryItem } from './HistoryItem';
 
-export function HistoryList(): JSX.Element{
+export function HistoryList(props: any): JSX.Element {
+    const { calculated, setCalculated } = props;
 
     const tips = [
         {
-            'id' : 1,
-            'overallAmount' : 400,
-            'quality' : "Špatná",
-            'peopleNo' : 3
+            id: 1,
+            overallAmount: 400,
+            quality: 'Špatná',
+            peopleNo: 3,
         },
         {
-            'id' : 2,
-            'overallAmount' : 520,
-            'quality' : "Dobrá",
-            'peopleNo' : 2
+            id: 2,
+            overallAmount: 520,
+            quality: 'Dobrá',
+            peopleNo: 2,
         },
         {
-            'id' : 3,
-            'overallAmount' : 765,
-            'quality' : "Perfektní, jsem nadšený",
-            'peopleNo' : 4
-        },]
+            id: 3,
+            overallAmount: 765,
+            quality: 'Perfektní, jsem nadšený',
+            peopleNo: 4,
+        }];
 
     const [isLoading, setIsLoading] = useState(true);
     const [loadedTips, setLoadedTips] = useState(tips);
 
     useEffect(() => {
-        fetch('https://tip-app-54c1b-default-rtdb.europe-west1.firebasedatabase.app/tips.json'
-        ).then(response => {
-            return response.json();
-        }).then(data =>{
-            const tips = [];
-
-            for(const key in data){
-                const tip = {
-                    id: key,
-                    ...data[key]
-                }
-                tips.push(tip)
-            }
+        fetch('https://tip-app-54c1b-default-rtdb.europe-west1.firebasedatabase.app/tips.json').then(
+            (response) => response.json(),
+        ).then((data) => {
+            const responseTips = Object.entries(data).map((dataEl: any) => ({ id: dataEl[0], ...dataEl[1] }));
 
             setIsLoading(false);
-            setLoadedTips(tips);
+            setLoadedTips(responseTips.reverse());
         });
-    }, [])
+        setCalculated(false);
+    }, [calculated, setCalculated]);
 
-
-    if(isLoading){
+    if (isLoading) {
         return (
             <h4>Načítá se historie dýšek.</h4>
-        )
-    }else{
-
+        );
     }
 
-    return(
+    return (
         <>
-        <Divider>
-            <h1>Historie dýšek</h1>
-        </Divider>
-            {loadedTips.map((tip: any) => {
-                return(
-                    <Divider key={tip.id}>
-                    <HistoryItem id={tip.id} key={tip.id} overallAmount={tip.finalResult} quality={tip.quality} peopleNo={tip.peopleNo}/>
-                    </Divider>
-                )
-            })}
+            <Divider>
+                <h1>Historie dýšek</h1>
+            </Divider>
+            {loadedTips.map((tip: any) => (
+                <Divider key={tip.id}>
+                    <HistoryItem
+                        id={tip.id}
+                        key={tip.id}
+                        overallAmount={tip.finalResult}
+                        quality={tip.quality}
+                        peopleNo={tip.peopleNo}
+                    />
+                </Divider>
+            ))}
         </>
-    )
-
+    );
 }
--- src/components/TipCalculator.tsx
+++ src/components/TipCalculator.tsx
@@ -1,15 +1,18 @@
 import { Space } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import TipForm from './form/TipForm';
 import { HistoryList } from './HistoryList';
 
 export function TipCalculator(): JSX.Element {
+    const [isCalculated, setIsCalculated] = useState(false);
+
     function submitTipDataHandler(tipData: any): void {
         fetch('https://tip-app-54c1b-default-rtdb.europe-west1.firebasedatabase.app/tips.json',
             {
                 method: 'POST',
                 body: JSON.stringify(tipData),
             });
+        setIsCalculated(true);
     }
 
     return (
@@ -17,7 +20,8 @@ export function TipCalculator(): JSX.Element {
             <Space size={200}>
                 <TipForm submitTipData={submitTipDataHandler} />
             </Space>
-            <HistoryList />
+            {console.log(isCalculated)}
+            <HistoryList calculated={isCalculated} setCalculated={setIsCalculated} />
         </Space>
     );
 }
